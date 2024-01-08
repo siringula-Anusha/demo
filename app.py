21331c54c4d0bdd943da3fb64782725d0067d14b
siringula-Anusha <147702956+siringula-Anusha@users.noreply.github.com>
2024-01-08 12:03:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ st.header("Types of Birds")
 col1,col2=st.columns(2)
 with col1:
      st.subheader("Parrot")
-     st.image("C:\5P6\parrot.jpg",caption="Parrot",width=300,use_column_width=True)
+     st.image("./parrot.jpg",caption="Parrot",width=300,use_column_width=True)
      st.write("Parrots are cute")
 with col2:
       st.subheader("Dove")
-      st.image("C:\5P6\dove.jpg",caption="Dove",width=300,use_column_width=True)
+      st.image("./dove.jpg",caption="Dove",width=300,use_column_width=True)
       st.write("Doves are cute")
