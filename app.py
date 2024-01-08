95f57baa9ce732b1596eb1bf34cff0c483fed561
siringula-Anusha <147702956+siringula-Anusha@users.noreply.github.com>
2024-01-08 12:01:52 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+st.set_page_config(page_title='Birds')
+st.header("Types of Birds")
+
+col1,col2=st.columns(2)
+with col1:
+     st.subheader("Parrot")
+     st.image("C:\5P6\parrot.jpg",caption="Parrot",width=300,use_column_width=True)
+     st.write("Parrots are cute")
+with col2:
+      st.subheader("Dove")
+      st.image("C:\5P6\dove.jpg",caption="Dove",width=300,use_column_width=True)
+      st.write("Doves are cute")
